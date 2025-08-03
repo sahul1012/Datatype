d6970f8c5e624781bacce54621ab119d8432fd45
sahul1012 <sahulcse38@gmail.com>
2025-08-03 14:04:45 +0530

    code added

---
+++ src/org/test/Details.java
@@ -0,0 +1,22 @@
+package org.test;
+
+public class Details {
+	private void userName() {
+		System.out.println("Username as Sahul1012");
+	}
+	private void changePassword() {
+		System.out.println("This is Sahul-Working on change password");
+	}
+	private void removeKart() {
+		System.out.println("This is Sahul-Working on remove Kart");
+	}
+	private void disablekart() {
+		System.out.println("This is Sahul-Working on disable kart");
+	}
+	private void gpayUPI() {
+		System.out.println("This is Sahul-Working on gpay");
+	}
+	private void cashOnDelivery() {
+		System.out.println("This is Sahul-working on cash");
+	}
+}
