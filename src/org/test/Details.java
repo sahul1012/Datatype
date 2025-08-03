65aff39176ad08e07fda023482caa5f98add8195
sahul1012 <sahulcse38@gmail.com>
2025-08-03 18:11:25 +0530

    new code added venkat

--- src/org/test/Details.java
+++ src/org/test/Details.java
@@ -19,4 +19,11 @@ public class Details {
 	private void cashOnDelivery() {
 		System.out.println("This is Sahul-working on cash");
 	}
+	private void addQTY() {
+		System.out.println("Branch Swithced Successfully");
+	}
+	private void changeColour() {
+		System.out.println("Colour Changed Successfully");
+	}
 }
+
